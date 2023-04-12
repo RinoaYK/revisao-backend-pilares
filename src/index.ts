423e59549bdd10cb5089cfff0162f760332308df
RinoaYK <rinoayk@gmail.com>
2023-04-11 22:23:01 -0300

    tasks-endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { db } from "./database/knex";
-import { TUserDB } from "./types";
+import { TTaskDB, TUserDB } from "./types";
 
 const app = express();
 
@@ -63,60 +63,67 @@ app.post("/users", async (req: Request, res: Response) => {
   try {
     const { id, name, email, password } = req.body;
 
-    if(typeof id !== "string"){
-        res.status(400)
-        throw new Error("'id' deve ser string")
-    }
-    
-    if(id.length < 4){
-        res.status(400)
-        throw new Error("'id' deve possuir pelo menos 4 caracteres")
+    if (typeof id !== "string") {
+      res.status(400);
+      throw new Error("'id' deve ser string");
     }
 
-    if(typeof name !== "string"){
-        res.status(400)
-        throw new Error("'name' deve ser string")
-    }
-    
-    if(name.length < 2){
-        res.status(400)
-        throw new Error("'name' deve possuir pelo menos 2 caracteres")
+    if (id.length < 4) {
+      res.status(400);
+      throw new Error("'id' deve possuir pelo menos 4 caracteres");
     }
 
-    if(typeof email !== "string"){
-        res.status(400)
-        throw new Error("'email' deve ser string")
+    if (typeof name !== "string") {
+      res.status(400);
+      throw new Error("'name' deve ser string");
     }
 
-    if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/g)) {
-        throw new Error("'password' deve possuir entre 8 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial")
+    if (name.length < 2) {
+      res.status(400);
+      throw new Error("'name' deve possuir pelo menos 2 caracteres");
     }
 
-    const [idExists]: TUserDB[] | undefined[] = await db("users").where({id})
-
-    if(idExists){
-        res.status(400)
-        throw new Error("'id' já existe")
+    if (typeof email !== "string") {
+      res.status(400);
+      throw new Error("'email' deve ser string");
     }
-    
-    const [emailExists]: TUserDB[] | undefined[] = await db("users").where({email})
 
-    if(emailExists){
-        res.status(400)
-        throw new Error("'email' já existe")
+    if (
+      !password.match(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/g
+      )
+    ) {
+      throw new Error(
+        "'password' deve possuir entre 8 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial"
+      );
+    }
+
+    const [idExists]: TUserDB[] | undefined[] = await db("users").where({ id });
+
+    if (idExists) {
+      res.status(400);
+      throw new Error("'id' já existe");
+    }
+
+    const [emailExists]: TUserDB[] | undefined[] = await db("users").where({
+      email,
+    });
+
+    if (emailExists) {
+      res.status(400);
+      throw new Error("'email' já existe");
     }
     const newUser: TUserDB = {
-        id,
-        name,
-        email,
-        password
-    }
-    await db("users").insert(newUser)  
+      id,
+      name,
+      email,
+      password,
+    };
+    await db("users").insert(newUser);
     res.status(201).send({
-        message: "User criado com sucesso", 
-        user:newUser
-    })
-
+      message: "User criado com sucesso",
+      user: newUser,
+    });
   } catch (error) {
     console.log(error);
 
@@ -136,19 +143,21 @@ app.post("/users", async (req: Request, res: Response) => {
 app.delete("/users/:id", async (req: Request, res: Response) => {
   try {
     const idToDelete = req.params.id;
-    
-    if(idToDelete[0] !== "f"){
-        res.status(400)
-        throw new Error("'id' deve iniciar com a letra 'f'")
-    }
-    
-    const [ userExists ] : TUserDB[] | undefined[] = await db("users").where({id:idToDelete})
 
-    if(!userExists){
-        res.status(404)
-        throw new Error("'id' não encontrado")
+    if (idToDelete[0] !== "f") {
+      res.status(400);
+      throw new Error("'id' deve iniciar com a letra 'f'");
     }
-    await db("users").del().where({id:idToDelete})
+
+    const [userExists]: TUserDB[] | undefined[] = await db("users").where({
+      id: idToDelete,
+    });
+
+    if (!userExists) {
+      res.status(404);
+      throw new Error("'id' não encontrado");
+    }
+    await db("users").del().where({ id: idToDelete });
 
     res.status(200).send({ message: "User deletado com sucesso" });
     // res.status(200).end();
@@ -169,9 +178,234 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
 
 //   Tasks
 //   GET all tasks
+app.get("/tasks", async (req: Request, res: Response) => {
+  try {
+    const q = req.query.q as string | undefined;
+
+    if (q === undefined) {
+      const result = await db("tasks");
+      res.status(200).send(result);
+    } else {
+      const result = await db("tasks")
+        .where("title", "LIKE", `%${q}%`)
+        .orWhere("description", "LIKE", `%${q}%`);
+
+      res.status(200).send(result);
+    }
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
+
 //   POST task
+app.post("/tasks", async (req: Request, res: Response) => {
+  try {
+    const { id, title, description } = req.body;
+
+    if (typeof id !== "string") {
+      res.status(400);
+      throw new Error("'id' deve ser string");
+    }
+
+    if (id.length < 4) {
+      res.status(400);
+      throw new Error("'id' deve possuir pelo menos 4 caracteres");
+    }
+
+    if (typeof title !== "string") {
+      res.status(400);
+      throw new Error("'title' deve ser string");
+    }
+
+    if (title.length < 2) {
+      res.status(400);
+      throw new Error("'title' deve possuir pelo menos 2 caracteres");
+    }
+
+    if (typeof description !== "string") {
+      res.status(400);
+      throw new Error("'description' deve ser string");
+    }
+
+    const [idExists]: TTaskDB[] | undefined[] = await db("tasks").where({ id });
+
+    if (idExists) {
+      res.status(400);
+      throw new Error("'id' já existe");
+    }
+
+    const newTask = {
+      id,
+      title,
+      description,
+    };
+
+    await db("tasks").insert(newTask);
+
+    const [insertTask]: TTaskDB[] = await db("tasks").where({ id });
+
+    res.status(201).send({
+      message: "Task criado com sucesso",
+      task: insertTask,
+    });
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
+
 //   PUT task by id
+app.put("/tasks/:id", async (req: Request, res: Response) => {
+  try {
+    const idToEdit = req.params.id;
+
+    // const { id, title, description, createdAt, status } = req.body;
+
+    const newId = req.body.id;
+    const newTitle = req.body.title;
+    const newDescription = req.body.description;
+    const newCreatedAt = req.body.createdAt;
+    const newStatus = req.body.status;
+
+    if (newId !== undefined) {
+      if (typeof newId !== "string") {
+        res.status(400);
+        throw new Error("'id' deve ser string");
+      }
+
+      if (newId.length < 4) {
+        res.status(400);
+        throw new Error("'id' deve possuir pelo menos 4 caracteres");
+      }
+    }
+
+    if (newTitle !== undefined) {
+      if (typeof newTitle !== "string") {
+        res.status(400);
+        throw new Error("'title' deve ser string");
+      }
+
+      if (newTitle.length < 2) {
+        res.status(400);
+        throw new Error("'title' deve possuir pelo menos 2 caracteres");
+      }
+    }
+
+    if (newDescription !== undefined) {
+      if (typeof newDescription !== "string") {
+        res.status(400);
+        throw new Error("'description' deve ser string");
+      }
+    }
+
+    if (newCreatedAt !== undefined) {
+      if (typeof newCreatedAt !== "string") {
+        res.status(400);
+        throw new Error("'createdAt' deve ser string");
+      }
+    }
+
+    if (newStatus !== undefined) {
+      if (typeof newStatus !== "number") {
+        res.status(400);
+        throw new Error("'status' deve ser number (0 ou 1)");
+      }
+    }
+
+    const [task]: TTaskDB[] | undefined[] = await db("tasks").where({
+      id: idToEdit,
+    });
+
+    if (!task) {
+      res.status(404);
+      throw new Error("'id' não encontrado");
+    }
+
+    const newTask: TTaskDB = {
+      id: newId || task.id,
+      title: newTitle || task.title,
+      description: newDescription || task.description,
+      created_at: newCreatedAt || task.created_at,
+      status: isNaN(newStatus) ? task.status : newStatus,
+    };
+
+    await db("tasks").update(newTask).where({ id: idToEdit });
+
+    res.status(201).send({
+      message: "Task editada com sucesso",
+      task: newTask
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
+
 //   DELETE task by id
+app.delete("/tasks/:id", async (req: Request, res: Response) => {
+    try {
+      const idToDelete = req.params.id;
+
+      if (idToDelete[0] !== "t") {
+        res.status(400);
+        throw new Error("'id' deve iniciar com a letra 't'");
+      }
+  
+      const [taskIdToDelete]: TTaskDB[] | undefined[] = await db("tasks").where({
+        id: idToDelete,
+      });
+  
+      if (!taskIdToDelete) {
+        res.status(404);
+        throw new Error("'id' não encontrado");
+      }
+      await db("tasks").del().where({ id: idToDelete });
+  
+      res.status(200).send({ message: "Task deletada com sucesso" });
+        
+    } catch (error) {
+      console.log(error);
+  
+      if (req.statusCode === 200) {
+        res.status(500);
+      }
+  
+      if (error instanceof Error) {
+        res.send(error.message);
+      } else {
+        res.send("Erro inesperado");
+      }
+    }
+  });
 
 //   Users + Tasks
 //   POST user to task by ids
